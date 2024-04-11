658bdaca90058b0ec51171ed43e12b63c9ea0844
Victor-dblasius <149601314+Victor-dblasius@users.noreply.github.com>
2024-04-11 16:48:12 -0300

    11

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataAtual = new Date();
-let dataNiver = prompt (" Data do próximo aniversario: ");
-dataNiver = new Date(dataNiver+"T23:59:59");
+let dataNiver = prompt (" Data do próximo aniversario:");
+dataNiver = new Date (dataNiver + "T23:59:59");
 let diasQueFaltam = Math.floor((dataNiver-dataAtual)/86400000);
-document.querySelector("#dias_restantes"). textContent = diasQueFaltam;
+document.querySelector("#dias_restantes").textContent = diasQueFaltam;
